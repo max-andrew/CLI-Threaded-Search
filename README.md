ca204ddc1bd380e2e06e97f3dfbcaa4bf255ad1d
maxwell-andrew <andrewmr@bc.edu>
2017-08-14 13:19:57 -0400

    Search & README

---
+++ README.md
@@ -0,0 +1,11 @@
+# CLI Threaded Search
+
+Search text entered in the command line for a given string using multiple threads. 
+
+### Initialization
+
+After compilation, run search.c with two arguments. These arguments are a query string that will be found in the main text and the size of the main text array, respectively. The main text can either be ported from a .txt file using the `<` command or entered into the command line as standard input after initializing  search. Lastly, enter `ctrl-d` when finished entering text.
+
+### Note
+
+This program is more a demonstration of threading in C than a robust search tool. Please note that the asynchronous nature of threads means the identified query location is not necessarily the first instance of the query string in the main text.
---
+++ search.c
@@ -0,0 +1,111 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+// create a fixed number of searcher threads
+#define NUM_THREADS 100
+
+void *searcher(void *); 
+
+/* add global mailboxes for thread communication */
+
+// location of beginning of pattern -- all threads quit when non-negative
+int location = -1;
+// thread counter
+int threadNext = 0; 
+// positions searched counter
+int visited = 0; 
+// the text array
+char *text;
+// the pattern
+char *pattern;
+// length of text string
+int textSize;
+// length of pattern string
+int patternSize;
+// init lock variable to prevent race conditions
+pthread_mutex_t lock;
+
+int main(int argc, char *argv[]) {
+
+	// first argument is the pattern string
+	pattern = argv[1];
+	patternSize = strlen(pattern);
+	// second argument is the size of the array that will hold the text string
+	textSize = atoi(argv[2]);
+
+	// allocate space for the text array and read the text characters into it
+	text = (char *)malloc(textSize + 1);
+	int n = 0;
+	// then read characters from the standard input, storing them in the text array 		until either an EOF is found or the array has been filled
+	while (n < textSize) {
+		int status = scanf("%c", &text[n]);
+		n++;
+		if (status == EOF) {
+			textSize = n;
+			break;
+		}
+	}
+	// add end line char at end
+	text[n] = '\0';
+
+	// lock critical section
+    pthread_mutex_init(&lock, NULL);
+	// create thread id
+    pthread_t tid[NUM_THREADS];
+    
+	// thread increment
+	int t;
+    for (t = 0; t < NUM_THREADS; t++) {
+        int status = pthread_create(&tid[t], NULL, searcher, NULL);
+	}
+
+	// wait for the searcher threads to finish
+	for (t=0; t < NUM_THREADS; t++)
+        pthread_join(tid[t], NULL);
+
+	// location of pattern found in text
+	if (location >= 0)
+		// print the result
+		printf("\nPattern begins at character %d\n", location);
+	else
+		printf("Pattern not found\n");
+}
+
+// behavior of a searcher thread
+void *searcher(void *arg) {
+	while (1) {
+		// lock critical section
+		pthread_mutex_lock(&lock);
+
+		// pick an unexamined starting position
+        int h = threadNext;
+		// repeat until either the pattern is found (by this thread or another one), 			// or there are no more unexamined starting positions
+		if (location >= 0 || threadNext > textSize-patternSize) {
+				pthread_mutex_unlock(&lock);
+			    break;
+		}
+		threadNext++;
+
+		pthread_mutex_unlock(&lock);  
+		// end of critical section
+		// found boolean val
+		int found = 1;
+		// counter
+		int k;
+		for (k = 0; k < patternSize; k++) {
+			if (text[h + k] != pattern[k])
+				found = 0;
+		}
+
+		// second critical section
+ 	    pthread_mutex_lock(&lock);  
+
+	    visited++;
+        if (found)
+   	 	   location = h+1;
+
+	   // end section, unlock
+	   pthread_mutex_unlock(&lock); 
+	}
+}
